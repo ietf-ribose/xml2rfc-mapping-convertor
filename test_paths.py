dc753dc6ead57e5c91a9627ca9f4e1df6985c855
Anton Strogonoff <anton@strogonoff.name>
2022-06-23 19:08:53 +0700

    chore(test_paths): fix issues & reorder units

--- test_paths.py
+++ test_paths.py
@@ -111,137 +111,6 @@ def test_xml2rfc_paths(
     raise typer.Exit(code=0)
 
 
-def create_reporter(
-    api_root: str,
-    dirname: str,
-    reports_root: str,
-    reference_root: Optional[str] = None,
-) -> Tuple[Callable[[str, PathOutcome], None], Callable[[], None]]:
-    _reports_dir = Path(reports_root)
-    stats_file = open(_reports_dir / f'{dirname}-stats.log', 'w')
-    report_file = open(_reports_dir / f'{dirname}-report.html', 'w')
-    report_file.truncate(0)
-    report_file.seek(0)
-    report_file.write(f'''<!doctype html>
-        <head>
-        <style>
-            body, html {{
-                padding: 0;
-                margin: 0;
-            }}
-            body {{
-                padding: 1em;
-                font-size: 14px;
-                line-height: 1.2;
-                font-family: sans-serif;
-            }}
-            h1 {{
-                font-size: 120%;
-            }}
-            pre.xml {{
-                white-space: pre-line;
-                max-width: 80vw;
-                overflow: auto;
-                background: whiteSmoke;
-                padding: 1em;
-            }}
-            .tools a {{
-                margin-right: 1em;
-            }}
-        </style>
-        <meta charset="utf-8">
-        <title>xml2rfc path report for {dirname} directory</title>
-        <body>
-        <h1>xml2rfc path report for {dirname} directory</h1>
-        <p>
-            Testing {api_root}
-            {"comparing with " if reference_root else ""}{reference_root or ""}
-        <p class="tools">
-            <a href="javascript:document.querySelectorAll('details').forEach(el => el.setAttribute('open', 'open'))">Open all</a>
-            <a href="javascript:document.querySelectorAll('details').forEach(el => el.removeAttribute('open'))">Close all</a>
-            <a href="javascript:document.querySelectorAll('details.path:not(.error)').forEach(el => el.style.display = 'none')">Hide successful paths</a>
-            <a style="{"display: none" if not reference_root else ""}" href="javascript:document.querySelectorAll('details.path:not(.has-diff)').forEach(el => el.style.display = 'none')">Hide paths w/o diff</a>
-            <a href="javascript:document.querySelectorAll('details.path').forEach(el => el.style.display = 'block')">Show all paths</a>
-        </p>
-        <details>
-            <summary>Processed paths</summary>
-    ''')
-
-    stats: Stats = Stats()
-    report: List[Tuple[str, PathOutcome]] = []
-
-    def destroy():
-        try:
-            stats_file.close()
-        except:
-            pass
-        try:
-            report_file.write('</details>')
-            report_file.write('<h2>Stats</h2>')
-            report_file.write(f'<pre>{yaml.dump(dataclasses.asdict(stats))}</pre>')
-        except:
-            pass
-        finally:
-            report_file.close()
-
-    def report_outcome(
-        subpath: str,
-        outcome: PathOutcome,
-    ):
-        basename = os.path.basename(subpath)
-        test_url = f"{api_root.removesuffix('/')}/{basename}"
-
-        if outcome.error:
-            outcome_desc = f'<p>Request failed with (error possibly truncated): <pre>{html.escape(outcome.error[:500])}</pre>'
-            stats.failed += 1
-        elif meth := outcome.successful_method:
-            outcome_desc = f'<p>{method_labels[meth.method]} succeeded'
-            if meth.method == 'manual':
-                stats.used_mapping += 1
-            elif meth.method == 'auto':
-                stats.used_auto_resolution += 1
-            elif meth.method == 'fallback':
-                stats.used_fallback += 1
-
-        if reference_root and outcome.reference:
-            ref_url = f"{reference_root.removesuffix('/')}/{basename}"
-            reference_link = f'<p>Comparing with reference: <a href="{ref_url}">{ref_url}</a>'
-        else:
-            reference_link = ''
-
-        if outcome.diff:
-            xml = f'<p>Diff of effective outcome against reference: <pre class="xml">{outcome.diff}</pre>'
-        elif outcome.reference:
-            xml = f'<details><summary>Obtained XML is identical to reference</summary><pre class="xml">{html.escape(outcome.resulting_xml or "XML N/A")}</pre></details>'
-        elif outcome.resulting_xml:
-            xml = f'<details><summary>Obtained XML</summary><pre class="xml">{html.escape(outcome.resulting_xml)}</pre></details>'
-        else:
-            xml = ''
-
-        report_file.write(f'''
-            <details class="path {"error" if outcome.error else "success"} {"has-diff" if outcome.diff else ""}">
-                <summary>
-                    {dirname} / {basename}
-                    — {"<strong>error ⚠️</strong>" if outcome.error else outcome.successful_method.method}
-                    {"— diff available" if outcome.diff else ""}
-                </summary>
-                <div style="padding: 0 1em 1em 1em;">
-                    <p>
-                        Attempted <a href="{test_url}">{test_url}</a>
-                        {outcome_desc}
-                    {reference_link}
-                    {xml}
-                </div>
-            </details>
-        ''')
-
-        stats_file.truncate(0)
-        stats_file.seek(0)
-        stats_file.write(yaml.dump(dataclasses.asdict(stats)))
-
-    return report_outcome, destroy
-
-
 def test_xml2rfc_dir(
     api_root: str,
     archive_root: str,
@@ -345,16 +214,24 @@ def test_xml2rfc_path(
             successful_method=successful_method,
         )
 
-    if reference_root and not outcome.error:
+    if reference_root:
         reference_url = f"{reference_root.removesuffix('/')}/{subpath}"
         reference_result = requests.get(reference_url)
         try:
             reference_result.raise_for_status()
         except requests.exceptions.HTTPError as err:
-            pass
+            if err.response.status_code == test_resp.status_code == 404:
+                outcome.error = (
+                    f'{outcome.error} — '
+                    'NOTE: reference check failed with 404 as well'
+                )
         else:
             outcome.reference = reference_result.text
-            if outcome.reference != outcome.resulting_xml:
+            if all([
+                outcome.resulting_xml,
+                outcome.reference,
+                outcome.reference != outcome.resulting_xml,
+            ]):
                 diffs = dmp.diff_main(
                     outcome.reference,
                     outcome.resulting_xml,
@@ -408,6 +285,142 @@ def unalias_dirname(dirname: str) -> List[str]:
         return [dirname, *XML2RFC_COMPAT_DIR_ALIASES[dirname]]
     else:
         raise ValueError("Unknown xml2rfc directory %s" % dirname)
+
+
+def create_reporter(
+    api_root: str,
+    dirname: str,
+    reports_root: str,
+    reference_root: Optional[str] = None,
+) -> Tuple[Callable[[str, PathOutcome], None], Callable[[], None]]:
+    _reports_dir = Path(reports_root)
+    stats_file = open(_reports_dir / f'{dirname}-stats.log', 'w')
+    report_file = open(_reports_dir / f'{dirname}-report.html', 'w')
+    report_file.truncate(0)
+    report_file.seek(0)
+    report_file.write(f'''<!doctype html>
+        <head>
+        <style>
+            body, html {{
+                padding: 0;
+                margin: 0;
+            }}
+            body {{
+                padding: 1em;
+                font-size: 14px;
+                line-height: 1.2;
+                font-family: sans-serif;
+            }}
+            h1 {{
+                font-size: 120%;
+            }}
+            pre.xml {{
+                white-space: pre-line;
+                max-width: 80vw;
+                overflow: auto;
+                background: whiteSmoke;
+                padding: 1em;
+            }}
+            .tools a {{
+                margin-right: 1em;
+            }}
+        </style>
+        <meta charset="utf-8">
+        <title>xml2rfc path report for {dirname} directory</title>
+        <body>
+        <h1>xml2rfc path report for {dirname} directory</h1>
+        <p>
+            Testing {api_root}
+            {"comparing with " if reference_root else ""}{reference_root or ""}
+        <p class="tools">
+            <a href="javascript:document.querySelectorAll('details').forEach(el => el.setAttribute('open', 'open'))">Open all</a>
+            <a href="javascript:document.querySelectorAll('details').forEach(el => el.removeAttribute('open'))">Close all</a>
+            <a href="javascript:document.querySelectorAll('details.path:not(.error)').forEach(el => el.style.display = 'none')">Hide successful paths</a>
+            <a style="{"display: none" if not reference_root else ""}" href="javascript:document.querySelectorAll('details.path:not(.has-diff)').forEach(el => el.style.display = 'none')">Hide paths w/o diff</a>
+            <a href="javascript:document.querySelectorAll('details.path').forEach(el => el.style.display = 'block')">Show all paths</a>
+        </p>
+        <details>
+            <summary>Processed paths</summary>
+    ''')
+
+    stats: Stats = Stats()
+    report: List[Tuple[str, PathOutcome]] = []
+
+    def destroy():
+        try:
+            stats_file.close()
+        except:
+            pass
+        try:
+            report_file.write('</details>')
+            report_file.write('<h2>Stats</h2>')
+            report_file.write(f'<pre>{yaml.dump(dataclasses.asdict(stats))}</pre>')
+        except:
+            pass
+        finally:
+            report_file.close()
+
+    def report_outcome(
+        subpath: str,
+        outcome: PathOutcome,
+    ):
+        basename = os.path.basename(subpath)
+        test_url = f"{api_root.removesuffix('/')}/{basename}"
+
+        if outcome.error:
+            outcome_label = '<strong>error ⚠️</strong>'
+            outcome_desc = f'<p>Request failed with (error possibly truncated): <pre>{html.escape(outcome.error[:500])}</pre>'
+            stats.failed += 1
+        elif meth := outcome.successful_method:
+            outcome_label = meth.method
+            outcome_desc = f'<p>{method_labels[meth.method]} succeeded'
+            if meth.method == 'manual':
+                stats.used_mapping += 1
+            elif meth.method == 'auto':
+                stats.used_auto_resolution += 1
+            elif meth.method == 'fallback':
+                stats.used_fallback += 1
+        else:
+            outcome_label = ''
+            outcome_desc = ''
+
+        if reference_root and outcome.reference:
+            ref_url = f"{reference_root.removesuffix('/')}/{basename}"
+            reference_link = f'<p>Comparing with reference: <a href="{ref_url}">{ref_url}</a>'
+        else:
+            reference_link = ''
+
+        if outcome.diff:
+            xml = f'<p>Diff of effective outcome against reference: <pre class="xml">{outcome.diff}</pre>'
+        elif outcome.reference:
+            xml = f'<details><summary>Obtained XML is identical to reference</summary><pre class="xml">{html.escape(outcome.resulting_xml or "XML N/A")}</pre></details>'
+        elif outcome.resulting_xml:
+            xml = f'<details><summary>Obtained XML</summary><pre class="xml">{html.escape(outcome.resulting_xml)}</pre></details>'
+        else:
+            xml = ''
+
+        report_file.write(f'''
+            <details class="path {"error" if outcome.error else "success"} {"has-diff" if outcome.diff else ""}">
+                <summary>
+                    {dirname} / {basename}
+                    — {outcome_label}
+                    {"— diff available" if outcome.diff else ""}
+                </summary>
+                <div style="padding: 0 1em 1em 1em;">
+                    <p>
+                        Attempted <a href="{test_url}">{test_url}</a>
+                        {outcome_desc}
+                    {reference_link}
+                    {xml}
+                </div>
+            </details>
+        ''')
+
+        stats_file.truncate(0)
+        stats_file.seek(0)
+        stats_file.write(yaml.dump(dataclasses.asdict(stats)))
+
+    return report_outcome, destroy
 
 
 if __name__ == '__main__':
