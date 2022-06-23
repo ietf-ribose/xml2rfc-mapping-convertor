493098e8c106ebb7de917b8b5f07b9a8e4a97078
Anton Strogonoff <anton@strogonoff.name>
2022-06-23 17:03:11 +0700

    feat: add path tester

--- README.rst
+++ README.rst
@@ -1,6 +1,4 @@
-This tiny script takes mappings in YAML format,
-such as given in https://github.com/ietf-ribose/bibxml-service/issues/133#issuecomment-1106762628,
-and creates/updates sidecar metadata files under given ``bibxml-data-archive`` root.
+Helper scripts for working with xml2rfc paths and mappings.
 
 Setup
 =====
@@ -11,12 +9,19 @@ Setup
     source env/bin/activate
     pip install -r requirements.txt
 
-Running
-=======
+update_sidecar_meta.py
+======================
 
-Simple example::
+This tiny script takes mappings in YAML format,
+such as given in https://github.com/ietf-ribose/bibxml-service/issues/133#issuecomment-1106762628,
+and creates/updates sidecar metadata files under given ``bibxml-data-archive`` root.
 
-    python update_sidecar_meta.py w3c-mapping-2.yaml /path/to/bibxml-data-archive/bibxml4
+Usage
+-----
+
+Example::
+
+    python3 update_sidecar_meta.py w3c-mapping-2.yaml /path/to/bibxml-data-archive/bibxml4
 
 With ``w3c-mapping-2.yaml`` as file containing mappings
 and second argument pointing to local copy
@@ -27,7 +32,7 @@ It won’t affect any of the XML files but it will create new sidecar YAML files
 with ``primary_docid`` pointing to respective values within the given mapping YAML.
 
 Notable behavior that may cause data loss
------------------------------------------
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 .. important::
 
@@ -38,3 +43,52 @@ Notable behavior that may cause data loss
 - Any orphaned sidecar metadata file (without corresponding XML file) is deleted (with stderr warning).
 - Preexisting docid mapping, if any for given mapped path, is overwritten (with stderr warning).
   The rest of sidecar metadata is preserved.
+
+test_paths.py
+=============
+
+This script goes through entries in ``bibxml-data-archive``
+and pulls xml2rfc API endpoint for each path, optionally comparing with a reference (e.g., ``xml2rfc.tools.ietf.org``)
+and reporting results.
+
+Usage
+-----
+
+Example::
+
+    python3 test_paths.py http://localhost:8000/public/rfc /path/to/bibxml-data-archive --dirname bibxml4 --verbosity 2 --reports-dir reports
+
+Arguments:
+
+- First argument is API endpoint to test (complete with ``/public/rfc/`` suffix)
+- Second argument is ``bibxml-data-archive`` root on your local machine
+- ``dirname`` indicates a directory to test
+- ``verbosity`` indicates verbosity level (default is 1)
+- ``reports-dir`` points to a directory where reports can be placed (directory must exist, can be relative).
+- ``randomize`` will cause paths to be processed at random, which means you can
+- ``reference-root`` will additionally hit this API endpoint
+- ``check-aliases`` will additionally check path aliases (e.g., ``bibxml-w3c`` will also be checked for dirname ``bibxml4``)
+
+Reports
+-------
+
+For each directory,
+
+- An HTML report is written with breakdown by path and stats at the end.
+- A simple log file is written with basic stats (failed count and counts by resolution method).
+
+.. note:: Both files are updated continuously and hitting ``Ctrl+C`` halfway will still leave usable results.
+
+
+Reference comparison
+--------------------
+
+Example with reference comparison::
+
+    python3 test_paths.py http://localhost:8000/public/rfc /path/to/bibxml-data-archive --dirname bibxml4 --reference-root http://xml2rfc.tools.ietf.org/public/rfc/ --verbosity 2 --reports-dir reports
+
+This will incur additional API request, but will show a diff if resulting XML is not the same
+(which is supposed to be the case since the main service API aims to provide newer data,
+serialized to bibxml on the fly from authoritative bibliographic source data).
+
+.. important:: There’s no throttling and endpoint provided via ``reference-root`` will be hammered many times during the test run.
---
+++ test_paths.py
@@ -0,0 +1,374 @@
+from typing import Callable, List, Tuple, Dict, Optional, Any, TextIO, cast
+import os
+import glob
+from pathlib import Path
+import dataclasses
+import html
+import random
+
+import yaml
+import typer
+import requests
+from tqdm import tqdm
+
+import diff_match_patch
+
+
+dmp = diff_match_patch.diff_match_patch()
+
+
+@dataclasses.dataclass
+class Stats:
+    failed: int = 0
+    used_fallback: int = 0
+    used_mapping: int = 0
+    used_auto_resolution: int = 0
+
+
+@dataclasses.dataclass
+class MethodOutcome:
+    method: str
+    success: bool
+    config: Optional[str] = None
+    error: Optional[str] = None
+
+
+@dataclasses.dataclass
+class PathOutcome:
+    resulting_xml: Optional[str]
+    error: Optional[str] = None
+    methods_tried: Optional[List[MethodOutcome]] = None
+    successful_method: Optional[MethodOutcome] = None
+    reference: Optional[str] = None
+    diff: Optional[str] = None
+
+
+# Copy this from bibxml-service settings.
+XML2RFC_COMPAT_DIR_ALIASES: Dict[str, List[str]] = {
+    'bibxml': ['bibxml-rfcs'],
+    'bibxml2': ['bibxml-misc'],
+    'bibxml3': ['bibxml-ids'],
+    'bibxml4': ['bibxml-w3c'],
+    'bibxml5': ['bibxml-3gpp'],
+    'bibxml6': ['bibxml-ieee'],
+    'bibxml7': ['bibxml-doi'],
+    'bibxml8': ['bibxml-iana'],
+    'bibxml9': ['bibxml-rfcsubseries'],
+    'bibxml-nist': [],
+}
+
+
+def test_xml2rfc_paths(
+    api_root: str,
+    archive_root: str,
+    dirname: Optional[str] = None,
+    reference_root: Optional[str] = None,
+    reports_dir: Optional[str] = None,
+    check_aliases: bool = False,
+    randomize: bool = False,
+    verbosity: int = 1,
+):
+    if dirname:
+        dirnames = [dirname]
+    else:
+        dirnames = os.listdir(archive_root)
+
+    outcomes: Dict[str, Dict[str, Any]] = dict()
+
+    for _dirname in dirnames:
+        if reports_dir:
+            report, destroy_reporter = create_reporter(
+                api_root,
+                _dirname,
+                reports_dir,
+                reference_root,
+            )
+        else:
+            report, destroy_reporter = None, None
+        try:
+            outcome = test_xml2rfc_dir(
+                api_root,
+                archive_root,
+                _dirname,
+                reference_root=reference_root,
+                on_outcome=report,
+                check_aliases=check_aliases,
+                randomize=randomize,
+                verbosity=verbosity,
+            )
+        except Exception as err:
+            typer.secho(
+                "Failed to test directory %s (%s)" % (dirname, err),
+                err=True,
+                fg='red')
+            continue
+        else:
+            outcomes[_dirname] = outcome
+        finally:
+            if destroy_reporter:
+                destroy_reporter()
+
+    raise typer.Exit(code=0)
+
+
+def create_reporter(
+    api_root: str,
+    dirname: str,
+    reports_root: str,
+    reference_root: Optional[str] = None,
+) -> Tuple[Callable[[str, PathOutcome], None], Callable[[], None]]:
+    _reports_dir = Path(reports_root)
+    stats_file = open(_reports_dir / f'{dirname}-stats.log', 'w')
+    report_file = open(_reports_dir / f'{dirname}-report.html', 'w')
+    report_file.truncate(0)
+    report_file.seek(0)
+    report_file.write('<!doctype html>')
+    report_file.write('<head>')
+    report_file.write('<meta charset="utf-8">')
+    report_file.write(f'<title>Report for {dirname}</title>')
+    report_file.write('<body>')
+    report_file.write("<a href=\"javascript:document.querySelectorAll('details').forEach(el => el.setAttribute('open', 'open'))\">Open all details</a>")
+    report_file.write("&emsp;•&emsp;")
+    report_file.write("<a href=\"javascript:document.querySelectorAll('details').forEach(el => el.removeAttribute('open'))\">Close all details</a>")
+    report_file.write("&emsp;•&emsp;")
+    report_file.write("<a href=\"javascript:document.querySelectorAll('details.path:not(.error)').forEach(el => el.style.display = 'none')\">Hide successful paths</a>")
+    report_file.write("&emsp;•&emsp;")
+    report_file.write("<a href=\"javascript:document.querySelectorAll('details.path').forEach(el => el.style.display = 'block')\">Show all paths</a>")
+    report_file.write('<details><summary>Processed paths</summary>')
+
+    stats: Stats = Stats()
+    report: List[Tuple[str, PathOutcome]] = []
+
+    def destroy():
+        try:
+            stats_file.close()
+        except:
+            pass
+        try:
+            report_file.write('</details>')
+            report_file.write('<h2>Stats</h2>')
+            report_file.write(f'<pre>{yaml.dump(dataclasses.asdict(stats))}</pre>')
+        except:
+            pass
+        finally:
+            report_file.close()
+
+    def report_outcome(
+        subpath: str,
+        outcome: PathOutcome,
+    ):
+        basename = os.path.basename(subpath)
+
+        report_file.write(f'<details class="path {"error" if outcome.error else "success"}"><summary>{basename}')
+        if outcome.error:
+            report_file.write('— <strong>error ⚠️</strong>')
+        elif outcome.successful_method:
+            report_file.write(f'— {outcome.successful_method.method}')
+        if outcome.diff:
+            report_file.write('— diff available')
+        report_file.write('</summary>')
+        report_file.write('<div style="padding: 0 1em 1em 1em;">')
+
+        test_url = f"{api_root.removesuffix('/')}/{basename}"
+        report_file.write(f'<p>Attempted <a href="{test_url}">{test_url}</a>')
+        if outcome.error:
+            report_file.write(f'<p>Request failed with error (possibly truncated): <pre>{html.escape(outcome.error[:500])}</pre>')
+            stats.failed += 1
+        elif meth := outcome.successful_method:
+            report_file.write(f'<p>{method_labels[meth.method]} succeeded')
+            if meth.method == 'manual':
+                stats.used_mapping += 1
+            elif meth.method == 'auto':
+                stats.used_auto_resolution += 1
+            elif meth.method == 'fallback':
+                stats.used_fallback += 1
+        if reference_root and outcome.reference:
+            ref_url = f"{reference_root.removesuffix('/')}/{basename}"
+            report_file.write(f'<p>Reference used: <a href="{ref_url}">{ref_url}</a>')
+        if outcome.diff:
+            report_file.write(f'<p>Diff of effective outcome against reference:')
+            report_file.write(f'<pre>{outcome.diff}</pre>')
+        elif outcome.reference:
+            report_file.write(f'<p>Obtained XML identical to reference.')
+
+        if outcome.resulting_xml and not outcome.diff:
+            report_file.write(f'<details><summary>XML</summary><pre>{html.escape(outcome.resulting_xml)}</pre></details>')
+
+        report_file.write('</div></details>')
+
+        stats_file.truncate(0)
+        stats_file.seek(0)
+        stats_file.write(yaml.dump(dataclasses.asdict(stats)))
+
+    return report_outcome, destroy
+
+
+def test_xml2rfc_dir(
+    api_root: str,
+    archive_root: str,
+    dirname: str,
+    on_outcome: Optional[Callable[[str, PathOutcome], None]],
+    reference_root: Optional[str] = None,
+    check_aliases: bool = False,
+    randomize: bool = False,
+    verbosity: int = 1,
+) -> Dict[str, PathOutcome]:
+    """
+    Goes through each file
+    under specified ``dirname`` under ``archive_root``,
+    hits the appropriate URLs
+    relative to ``api_root`` and ``reference_root``,
+    and compares results.
+
+    Returns a dictionary that maps each filename
+    to either error
+    """
+
+    if check_aliases:
+        dirnames = unalias_dirname(dirname)
+        if verbosity > 1:
+            typer.echo("Dirname %s unpacked to include %s" % (
+                dirname,
+                ', '.join(dirnames),
+            ))
+    else:
+        dirnames = [dirname]
+
+    xml_files = glob.glob(f'{archive_root}/{dirname}/*.xml')
+
+    if randomize:
+        random.shuffle(xml_files)
+
+    outcomes: Dict[str, PathOutcome] = dict()
+
+    for alias in dirnames:
+        for xml_fname in tqdm(xml_files, desc="Checking paths in %s" % alias):
+            basename = os.path.basename(xml_fname)
+            basename_noext = os.path.splitext(basename)[0]
+            outcome = test_xml2rfc_path(
+                f'{dirname}/{basename}',
+                api_root,
+                reference_root,
+            )
+            if on_outcome:
+                on_outcome(xml_fname, outcome)
+            outcomes[xml_fname] = outcome
+
+    return outcomes
+
+
+def test_xml2rfc_path(
+    subpath: str,
+    api_root: str,
+    reference_root: Optional[str] = None,
+) -> PathOutcome:
+    """
+    Tests a given subpath by:
+
+    1. Appending ``subpath`` to ``api_root``
+    2. Requesting resulting URL and ensuring HTTP 200 with valid XML
+       is obtained
+    3. If ``reference_root`` is given, appending ``subpath``
+       to it and diffing the resulting XML with XML in step (2)
+
+    May take a while since it makes an actual request.
+
+    :raises:
+        Passes through any exception from :mod:`requests`.
+
+    :raises ValueError:
+        If HTTP 200 did not obtain or XML is not valid.
+
+    :returns:
+        2-tuple (outcome, diff) where diff will be None
+        if request to reference fails or if ``reference_root`` is not given.
+    """
+
+    test_url = f"{api_root.removesuffix('/')}/{subpath}"
+    test_resp = requests.get(
+        test_url,
+        headers={'X-Requested-With': 'xml2rfcResolver'})
+
+    try:
+        test_resp.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        err_resp = err.response
+        outcome = PathOutcome(
+            resulting_xml=None,
+            error=f'HTTP {err_resp.status_code}: {err_resp.text}',
+            methods_tried=get_methods_tried(err_resp.headers)[0],
+        )
+    else:
+        methods_tried, successful_method = get_methods_tried(test_resp.headers)
+        outcome = PathOutcome(
+            resulting_xml=test_resp.text,
+            methods_tried=methods_tried,
+            successful_method=successful_method,
+        )
+
+    if reference_root and not outcome.error:
+        reference_url = f"{reference_root.removesuffix('/')}/{subpath}"
+        reference_result = requests.get(reference_url)
+        try:
+            reference_result.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            pass
+        else:
+            outcome.reference = reference_result.text
+            if outcome.reference != outcome.resulting_xml:
+                diffs = dmp.diff_main(
+                    outcome.resulting_xml,
+                    outcome.reference)
+                dmp.diff_cleanupSemantic(diffs)
+                outcome.diff = dmp.diff_prettyHtml(diffs)
+
+    return outcome
+
+
+def get_methods_tried(headers: Dict[str, Any]) -> Tuple[
+    List[MethodOutcome],
+    Optional[MethodOutcome],
+]:
+    """
+    Given HTTP response headers, returns a 2-tuple of a list of method outcomes
+    and the successful outcome, if any.
+    """
+    methods_tried = []
+    successful_method = None
+
+    if ';' in headers.get('x-resolution-methods', ''):
+        methods = headers['x-resolution-methods'].split(';')
+        outcomes = headers.get('x-resolution-outcomes', '').split(';')
+        if len(methods) == len(outcomes):
+            for method, outcome in zip(methods, outcomes):
+                if ',' in outcome:
+                    config, err = outcome.split(',')
+                    outcome = MethodOutcome(
+                        method=method,
+                        success=err == '',
+                        config=config if config != '' else None,
+                        error=err if err != '' else None,
+                    )
+                    methods_tried.append(outcome)
+                    if not err:
+                        successful_method = outcome
+
+    return methods_tried, successful_method
+
+
+method_labels = {
+    'auto': "Automatic resolution",
+    'manual': "Mapping by primary docid",
+    'fallback': "Fallback to bibxml data archive",
+}
+
+
+def unalias_dirname(dirname: str) -> List[str]:
+    if dirname in XML2RFC_COMPAT_DIR_ALIASES:
+        return [dirname, *XML2RFC_COMPAT_DIR_ALIASES[dirname]]
+    else:
+        raise ValueError("Unknown xml2rfc directory %s" % dirname)
+
+
+if __name__ == '__main__':
+    typer.run(test_xml2rfc_paths)
